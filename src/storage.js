1156f264d94d5f659ab1b1abedfebc8a29fef9e0
Andrii <andrii@fluidweb.io>
2021-03-16 17:29:49 +0200

    fixed

--- src/storage.js
+++ src/storage.js
@@ -35,7 +35,7 @@ const defaultBoardData = {
     position: 1,
     list_title: "done",
     cards: {
-      "63bdd1d2-aa55-4e69-8f98-b345b5b6bdfd": {
+      "Для отримання наступного завдання напиши боту: /task 2fde284e-866c-11eb-8dcd-0242ac130003": {
         position: 0,
         card_content: "some data here"
       },
